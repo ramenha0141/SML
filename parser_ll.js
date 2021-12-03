d97e293bcc09f15e573196b1e68a80d3d4dce89e
BuildTools <unconfigured@null.spigotmc.org>
2021-12-03 14:01:49 +0900

    change identifier

--- parser_ll.js
+++ parser_ll.js
@@ -5,9 +5,9 @@ module.exports = parser = function (tokens, parsing_table) {
     const state = 0;
     while (stack.length > 0) {
         if (typeof stack[0] === 'object') {
-            const non_terminal_symbol = stack.shift()[0];
-            stack.unshift(...parsing_table[non_terminal_symbol][tokens[0]]);
-            rules.push(non_terminal_symbol);
+            const symbol = stack.shift()[0];
+            stack.unshift(...parsing_table[symbol][tokens[0]]);
+            rules.push(symbol);
         } else {
             const token = stack.shift();
             if (tokens[0] === token) {
