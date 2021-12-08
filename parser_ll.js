1edf037936b367d1b465d15ada5a925ca49848c6
BuildTools <unconfigured@null.spigotmc.org>
2021-12-08 19:17:46 +0900

    update

--- parser_ll.js
+++ parser_ll.js
@@ -1,40 +1,48 @@
 module.exports = parser = function (tokens, parsing_table) {
-    const stack = [['S'], ['$']];
+    const stack = [['S']];
     const rules = [];
+    tokens.push({type:'@$',value:''});
     while (stack.length > 0) {
-        if (stack[0][0] === '$') {
+        if (typeof stack[0] === 'object') {
             const symbol = stack.shift();
-            if (tokens[0][0] === '$') {
-                return rules;
+            if (symbol[0].slice(0,1) === '@') {
+                if (tokens[0].type === symbol[0]) {
+                    rules.push(tokens.shift().value);
+                } else {
+                    console.error(`${tokens.shift().value} token type is not ${symbol[0]}`);
+                }
             } else {
-                console.error('Parsing Error');
+                if (tokens[0].type === undefined) {
+                    stack.unshift(...parsing_table[symbol[0]][tokens[0].value]);
+                    rules.push(symbol);
+                } else {
+                    stack.unshift(...parsing_table[symbol[0]][tokens[0].type]);
+                    rules.push(symbol);
+                }
             }
-        } else if (!(typeof stack[0] === 'string')) {
-            const symbol = stack.shift();
-            stack.unshift(...parsing_table[symbol[0]][tokens[0]]);
-            rules.push(symbol);
         } else {
-            const token = stack.shift();
-            if (tokens[0] === token) {
-                rules.push(tokens.shift());
+            const symbol = stack.shift();
+            if (tokens[0].value === symbol) {
+                rules.push(tokens.shift().value);
             } else {
-                console.error(`${tokens.shift()} != ${token}`);
+                console.error(`${tokens.shift().value} token is not ${symbol}`);
             }
         }
     }
-    return rules;
+    return rules.slice(0, -1);
 }
-const tokens = ['a' ,'b', 'c', ['$']];
+const tokens = [{type:'@number',value:'1'},{value:'+'},{type:'@number',value:'2'},{value:'-'},{type:'@number',value:'3'}];
 const parsing_table = {
     'S' : {
-        'a' : [['A']],
-        'b' : [['B']]
+        '@number' : [['exp']]
     },
-    'A' : {
-        'a' : ['a', ['B']]
+    'exp' : {
+        '@number' : [['@number'], ['exp_']]
     },
-    'B' : {
-        'b' : ['b', 'c']
+    'exp_' : {
+        '+' : ['+', ['@number'], ['exp_']],
+        '-' : ['-', ['@number'], ['exp_']],
+        '@$' : [['@$']]
     }
 };
 console.log(parser(tokens, parsing_table));
