4f90abe0c0dc903a1a94b75783c53c1884c7a5c5
ramenha0141 <ramenhaoishii@gmail.com>
2021-12-17 21:23:41 +0900

    update

--- parser_ll.js
+++ parser_ll.js
@@ -7,10 +7,10 @@ module.exports = parser = function (tokens, parsing_table) {
             // 非終端記号
             const symbol = stack.shift();
             if (tokens[0].type === undefined) {
-                stack.unshift(...parsing_table[symbol[0]][tokens[0].value]);
+                stack.unshift(...parsing_table[symbol[0]][tokens[0].value], undefined);
                 rules.push(symbol);
             } else {
-                stack.unshift(...parsing_table[symbol[0]][Symbol.for(tokens[0].type)]);
+                stack.unshift(...parsing_table[symbol[0]][Symbol.for(tokens[0].type)], undefined);
                 rules.push(symbol);
             }
         } else if (typeof stack[0] === 'string') {
@@ -21,6 +21,8 @@ module.exports = parser = function (tokens, parsing_table) {
             } else {
                 console.error(`${tokens.shift().value} token is not ${symbol}`);
             }
+        } else if (stack[0] === Symbol.for('$')) {
+            stack.shift();
         } else if (typeof stack[0] === 'symbol') {
             // 特殊終端記号
             const symbol = stack.shift();
@@ -35,7 +37,26 @@ module.exports = parser = function (tokens, parsing_table) {
             rules.push([undefined]);
         }
     }
-    return rules;
+    console.log(rules);
+    function tree() {
+        const st = [];
+        while (rules.length > 0) {
+            if (typeof rules[0] === 'string') {
+                st.push(rules.shift());
+            } else if (typeof rules[0][0] === 'string') {
+                rules.shift();
+                const _st = tree();
+                if (_st.length > 0) {
+                    st.push(_st);
+                }
+            } else if (rules[0][0] === undefined) {
+                rules.shift();
+                return st;
+            }
+        }
+        return st;
+    }
+    return JSON.stringify(tree());
 }
 const $ = {number: Symbol.for('number'), $: Symbol.for('$')};
 const tokens = [{type:'number',value:'1'},{value:'+'},{type:'number',value:'2'},{value:'-'},{type:'number',value:'3'}];
