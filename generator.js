4ad90fda09b93462b8bc584a7fb14cd039e79349
ramenha0141 <ramenhaoishii@gmail.com>
2021-12-19 20:56:24 +0900

    update

--- generator.js
+++ generator.js
@@ -1,3 +1,11 @@
 module.exports = function Generator(ast) {
-    
+    function first(define) {
+        const A = ast[define];
+        const firsts = [];
+        for (let i = 0; i < A.length; i++) {
+            if (A[i] === 'ε') {
+                firsts.push('ε');
+            }
+        }
+    }
 }
