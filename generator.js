40141cd2a050a346f029fca60641bced8cc5602b
ramenha0141 <ramenhaoishii@gmail.com>
2022-01-05 18:23:00 +0900

    update

--- generator.js
+++ generator.js
@@ -4,7 +4,7 @@ module.exports = function Generator(ast) {
         if (typeof symbol_or_string === 'symbol') {
             return `Symbol(${Symbol.keyFor(symbol_or_string)})`;
         }
-        return symbol_or_string;
+        return `'${symbol_or_string}'`;
     }
     const parsing_table = {};
     const ast_keys = Object.keys(ast);
@@ -20,7 +20,7 @@ module.exports = function Generator(ast) {
                 if (parsing_table[ast_keys[i]][firsts[k]] === undefined) {
                     parsing_table[ast_keys[i]][firsts[k]] = s;
                 } else {
-                    throw `Generation Error: This rule cannot parse by LL(1).\nRule '${ast_keys[i]}', Lookahead'${symbol_wrap(firsts[k])}', Conflict between ${JSON.stringify(parsing_table[ast_keys[i]][firsts[k]])} and ${JSON.stringify(s)}.`;
+                    throw `Generation Error: This rule cannot parse by LL(1).\nRule '${ast_keys[i]}', Lookahead ${symbol_wrap(firsts[k])}, Conflict between ${JSON.stringify(parsing_table[ast_keys[i]][firsts[k]])} and ${JSON.stringify(s)}.`;
                 }
             }
         }
@@ -30,7 +30,7 @@ module.exports = function Generator(ast) {
                 if (parsing_table[ast_keys[i]][follows[j]] === undefined) {
                     parsing_table[ast_keys[i]][follows[j]] = [];
                 } else {
-                    throw `Generation Error: This rule cannot parse by LL(1).\nRule '${ast_keys[i]}', Lookahead'${symbol_wrap(follows[j])}', Conflict between ${JSON.stringify(parsing_table[ast_keys[i]][follows[j]])} and ${JSON.stringify([])}.`;
+                    throw `Generation Error: This rule cannot parse by LL(1).\nRule '${ast_keys[i]}', Lookahead ${symbol_wrap(follows[j])}, Conflict between ${JSON.stringify(parsing_table[ast_keys[i]][follows[j]])} and ${JSON.stringify([])}.`;
                 }
             }
         }
